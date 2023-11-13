cf8858055080226aadda3ff43524fbc15878e2a7
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-13 22:38:37 +0530

    added Attendance.java and main.java programs

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ package EmployyeWage;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Display.welcomeMessage);
+
+        Attendance.checkAttendance("Employee1");
+        Attendance.checkAttendance("Employee2");
     }
 }
